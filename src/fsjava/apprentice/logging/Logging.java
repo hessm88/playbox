a815e12f5543f24230ab915b190fefc35e892b4f
hessm88 <mhess@familysearch.org>
2014-11-07 10:35:41 -0700

    add logging outline

---
+++ src/fsjava/apprentice/logging/Logging.java
@@ -0,0 +1,9 @@
+public class Logging {
+
+  private static final Logger logger = LoggerFactory.getLogger();
+  private static final String MY_MESSAGE = "my message";
+
+  public static void main(String ... args) {
+
+  }
+}
