aefe9cf046ade60266a7ad13357199c3e43f199f
hessm88 <mhess@familysearch.org>
2014-11-07 10:50:23 -0700

    More logging work

--- fsjava-badge/src/main/java/apprentice/logging/Logging.java
+++ fsjava-badge/src/main/java/apprentice/logging/Logging.java
@@ -1,9 +1,15 @@
+package apprentice.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Logging {
 
-  private static final Logger logger = LoggerFactory.getLogger();
+  private static final Logger logger = LoggerFactory.getLogger(Logging.class);
   private static final String MY_MESSAGE = "my message";
 
   public static void main(String ... args) {
-
+    logger.info("Here I am {}", "available in http");
+    logger.debug("DEBUG!");
   }
 }
