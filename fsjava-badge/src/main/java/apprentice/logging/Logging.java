19f979ba5fe1e9d81b21a04a5e1895d42c82672f
hessm88 <mhess@familysearch.org>
2014-11-10 12:56:20 -0700

    Add initial logging changes

--- fsjava-badge/src/main/java/apprentice/logging/Logging.java
+++ fsjava-badge/src/main/java/apprentice/logging/Logging.java
@@ -6,10 +6,12 @@ import org.slf4j.LoggerFactory;
 public class Logging {
 
   private static final Logger logger = LoggerFactory.getLogger(Logging.class);
-  private static final String MY_MESSAGE = "my message";
 
   public static void main(String ... args) {
-    logger.info("Here I am {}", "available in http");
-    logger.debug("DEBUG!");
+    final String className = "Logging";
+    logger.error("Logger reports error in {}", className);
+    logger.warn("Logger reports warn in {}", className);
+    logger.info("Logger reports info in {}", className);
+    logger.debug("Logger reports DEBUG in {}", className);
   }
 }
